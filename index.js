e02422be5680cf64a8516a93584f686ac66d7a1d
Daniel Curtin <dcurtin.work@gmail.com>
2023-07-14 10:26:37 -0600

    Add posting new idea functionality

--- index.js
+++ index.js
@@ -20,6 +20,31 @@ app.get('/api/v1/ideas', (request, response) => {
   return response.status(200).json({ ideas: app.locals.ideas });
 });
 
+//Add an idea
+app.post('/api/v1/ideas', (request, response) => {
+  const newIdea = request.body;
+  const requiredParams = ['name', 'desc'];
+  let missingParams = [];
+
+  for (let requiredProperty of requiredParams) {
+    if (!newIdea[requiredProperty]) {
+      missingParams = [...missingParams, requiredProperty];
+    };
+  };
+
+  if (missingParams.length) {
+    return response.status(422).send({ message: `Missing ${missingParams.join(', ')} in request.`});
+  } else {
+    if (!app.locals.ideas.length) {
+      newIdea.id = 1;
+    } else {
+      newIdea.id = app.locals.ideas[app.locals.ideas.length - 1].id + 1;
+    }
+  }
+  app.locals.ideas.push(newIdea);
+  return response.status(201).json(newIdea);
+});
+
 app.listen(app.get('port'), () => {
   console.log('FS Ideabox API running on http://localhost:3001')
 });
